1cdefb8499336768ca8009c92a8369319dcd2c51
sdikyarts <wtrcrss.family@gmail.com>
2025-07-31 06:25:56 +0700

    Refactor: Add MEDIA_URL and MEDIA_ROOT settings for media file handling

--- musidoku_project/settings.py
+++ musidoku_project/settings.py
@@ -139,6 +139,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
