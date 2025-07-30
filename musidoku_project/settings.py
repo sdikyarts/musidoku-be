085b6d260885f8f2f714428918a12ccdf1547b2c
sdikyarts <wtrcrss.family@gmail.com>
2025-07-31 06:06:56 +0700

    Fix: Update and collect static

--- musidoku_project/settings.py
+++ musidoku_project/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
