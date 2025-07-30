39671aa3a1ac6989b496a6ea0ece8d25ee471b3c
sdikyarts <wtrcrss.family@gmail.com>
2025-07-31 06:50:41 +0700

    Refactor: Update STATIC_URL and MEDIA_URL to include leading slashes; adjust urlpatterns for static and media file serving

--- musidoku_project/urls.py
+++ musidoku_project/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, include
-from django.conf.urls.static import static
+from django.urls import include, path
+from django.urls import re_path
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
