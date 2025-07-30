c351d799edff0f25694301fd0592d85471285c68
sdikyarts <wtrcrss.family@gmail.com>
2025-07-31 06:27:17 +0700

    Refactor: Remove unused API URL path from urlpatterns

--- musidoku_project/urls.py
+++ musidoku_project/urls.py
@@ -21,6 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('main.api.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
