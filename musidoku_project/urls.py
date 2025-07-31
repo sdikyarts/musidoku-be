ced95e6299b324f02fc303e2f0c98bb92faf697a
sdikyarts <wtrcrss.family@gmail.com>
2025-07-31 07:10:49 +0700

    Fix: Add build script and update Vercel configuration for static file handling

--- musidoku_project/urls.py
+++ musidoku_project/urls.py
@@ -19,7 +19,11 @@ from django.urls import include, path
 from django.urls import re_path
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
