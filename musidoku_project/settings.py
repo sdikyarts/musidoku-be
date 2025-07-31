b36009b7d56b66f708b3090fb48f4d97437dfe0f
sdikyarts <wtrcrss.family@gmail.com>
2025-07-31 07:14:40 +0700

    Refactor: Remove build script from Vercel configuration; add Whitenoise storage for static files

--- musidoku_project/settings.py
+++ musidoku_project/settings.py
@@ -141,6 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
